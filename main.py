df0809a36d0998c0f9a2ef307cdc361696a435bb
Victor Pedrota <58073023+vpedrota@users.noreply.github.com>
2022-07-06 11:01:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     name = request.args.get('name', 'World')
-    return f'oLÁ {name}!'
+    return f'oLÁS {name}!'
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 80)))
