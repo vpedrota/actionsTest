39f31a11d953e5717d9c2333d8b16a172b463654
Victor Pedrota <58073023+vpedrota@users.noreply.github.com>
2022-07-06 11:11:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     name = request.args.get('name', 'World')
-    return f'oLÁS {name}!'
+    return f'oLjÁS {name}!'
 
 
 if __name__ == "__main__":
